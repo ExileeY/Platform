3ed2f9d0c5dff8283bb10bac92c5b82fec08cd28
Danila Glazov <danikglazov@gmail.com>
2020-02-07 22:56:57 +0300

    Comments with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
 	before_action :load_entities
 	def create
-		@comment = @user.comments.new(comment_params)
-   		@comment.project = Project.find(params[:project_id])
-   		@comment.save
-      	redirect_back(fallback_location: project_path(@comment.project))
+			@comment = @user.comments.new(comment_params)
+	   		@comment.save
+	   		@comments = Comment.where(project_id: @comment.project_id)
+      	#redirect_back(fallback_location: project_path(@comment.project))
 	end
 
 	private
 
 	def comment_params
-		params.require(:comment).permit(:body, :user_id)
+		params.slice(:project_id).merge(params.require(:comment).permit(:body)).permit!
 	end
 
 	def load_entities
