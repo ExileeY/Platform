bd0bd8eedef4876dd9ed78eb591f793d30712246
Danila Glazov <danikglazov@gmail.com>
2020-02-16 21:01:01 +0300

    Destroy/Edit comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
+  	before_action :authenticate_user!, only: [:create]
 	before_action :load_entities
+	before_action :require_permission, only:[:edit, :update, :destroy]
 
 
 	def index
@@ -12,6 +14,27 @@ class CommentsController < ApplicationController
 	   		@comment.save
 	   		load_comments
       	#redirect_back(fallback_location: project_path(@comment.project))
+	end
+
+	def edit
+		@comment = Comment.find(params[:id])
+		@project = @comment.project
+	end
+
+	def update
+		@comment = Comment.find(params[:id])
+		@project = @comment.project
+		if @comment.update_attributes(comment_params)
+			flash[:success] = "Comment has been updated"
+			redirect_to project_path(@project)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
 	end
 
 	private
@@ -27,4 +50,10 @@ class CommentsController < ApplicationController
 	def load_comments
 		@comments = Comment.where(project_id: params[:project_id]).order("id desc").limit(10).reverse
 	end
+
+	def require_permission
+      if current_user != Comment.find(params[:id]).user
+        redirect_to project_path(Comment.find(params[:id]).project)
+      end
+    end
 end
